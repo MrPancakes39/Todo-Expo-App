d0a425eb8ca77c0cf1af9b871355aa9916db8eff
MrPancakes39 <salmannhassan39@gmail.com>
2024-06-21 22:16:16 +0300

    Fixed Not Found Redirecting

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -2,12 +2,19 @@ import { Link } from "expo-router";
 import { Text } from "react-native";
 
 import { Container } from "~/components/Container";
+import { useAuth } from "~/lib/auth/useAuth";
 
 export default function NotFoundScreen() {
+  const { isAuthenticated } = useAuth();
+
+  const homeRoute = isAuthenticated ? "/(protected)/" : "/";
+
+  console.log({ homeRoute });
+
   return (
     <Container>
       <Text className="text-xl font-bold">This screen doesn't exist.</Text>
-      <Link replace href="/" className="mt-4 pt-4">
+      <Link replace href={homeRoute} className="mt-4 pt-4">
         <Text className="text-base text-[#2e78b7]">Go to home screen!</Text>
       </Link>
     </Container>
