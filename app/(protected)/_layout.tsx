e3cf732db0632de5dc31ca7c5c43dc16f85b4030
MrPancakes39 <salmannhassan39@gmail.com>
2024-06-21 22:16:28 +0300

    Added a Tabs Layout

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -1,5 +1,27 @@
-import { Slot } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+import { Tabs } from "expo-router";
 
 export default function ProtectedLayout() {
-  return <Slot />;
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "white",
+        tabBarActiveBackgroundColor: "#18181b",
+        tabBarIconStyle: {
+          marginTop: 5,
+        },
+        tabBarLabelStyle: {
+          marginBottom: 5,
+        },
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          title: "Todos",
+          tabBarIcon: ({ color }) => <Feather name="edit-3" size={24} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
 }
--- app/(protected)/index.tsx
+++ app/(protected)/index.tsx
@@ -1,24 +1,11 @@
 import { Text } from "react-native";
 
-import { Button } from "~/components/Button";
 import { Container } from "~/components/Container";
-import { useAuth } from "~/lib/auth/useAuth";
 
-export default function TodoPage() {
-  const { signOut } = useAuth();
-
+export default function Page() {
   return (
-    <Container className="items-center justify-center">
-      <Text className="text-bold mb-4 text-center text-2xl">You are logged in</Text>
-      <Button
-        btnStyle={{
-          bg: ["bg-red-400", "bg-red-500"],
-          border: ["border-red-700", "border-red-700"],
-          text: "text-white",
-        }}
-        onPress={signOut}>
-        Sign Out
-      </Button>
+    <Container className="bg-black pt-8">
+      <Text className="text-center text-2xl font-bold text-white">List of Todos</Text>
     </Container>
   );
 }
